52b1b3e7f0b98e061c58ba8263dac0bb60ec7a67
FallingLights <FallingLights@protonmail.com>
2023-03-06 08:31:51 +0100

    Added MQTT listener

--- src/config/config.go
+++ src/config/config.go
@@ -1,9 +1,45 @@
 package config
 
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+func ReadDB() Configurations {
+	// Set the file name of the configurations file
+	viper.SetConfigName("config")
+	// Add the path to look for the configurations file
+	viper.AddConfigPath(".")
+	// Set the file type of the configurations file
+	viper.SetConfigType("yaml")
+
+	// Enable VIPER to read Environment Variables
+	viper.AutomaticEnv()
+
+	var configuration Configurations
+
+	// Read in the configurations file and check for errors
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Printf("Error reading config file, %s\n", err)
+		panic(err)
+	}
+
+	// set default values
+	viper.SetDefault("database.dbname", "test")
+
+	if err := viper.Unmarshal(&configuration); err != nil {
+		fmt.Printf("Unable to decode into struct, %v", err)
+		panic(err)
+	}
+
+	return configuration
+}
+
 // Configurations exported
 type Configurations struct {
 	Server       ServerConfigurations
 	Database     DatabaseConfigurations
+	Mqtt         MqttConfigurations
 	EXAMPLE_PATH string
 	EXAMPLE_VAR  string
 }
@@ -18,4 +54,13 @@ type DatabaseConfigurations struct {
 	DBName     string
 	DBUser     string
 	DBPassword string
+}
+
+type MqttConfigurations struct {
+	Broker   string
+	Port     int
+	ClientID string
+	Username string
+	Password string
+	Topic    string
 }
--- src/main.go
+++ src/main.go
@@ -4,11 +4,26 @@ import (
 	"fmt"
 	c "github.com/Grid-instruments/mqtt-to-postgresql/src/config"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
-	"github.com/spf13/viper"
+	"os"
+	"strings"
+	"time"
 )
 
+var flag bool = false
+
 var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+	//fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+	topic := msg.Topic()
+	payload := msg.Payload()
+	if strings.Compare(string(payload), "\n") > 0 {
+		fmt.Printf("TOPIC: %s\n", topic)
+		fmt.Printf("MSG: %s\n", payload)
+	}
+
+	if strings.Compare("bye\n", string(payload)) == 0 {
+		fmt.Println("exitting")
+		flag = true
+	}
 }
 
 var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
@@ -19,34 +34,55 @@ var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err
 	fmt.Printf("Connect lost: %v", err)
 }
 
+func sub(client mqtt.Client, topic string) {
+	if token := client.Subscribe(topic, 0, nil); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+	fmt.Println("Subscribed to topic: ", topic)
+}
+
+func sub2(client mqtt.Client, topic string) {
+	token := client.Subscribe(topic, 1, nil)
+	token.Wait()
+	fmt.Printf("Subscribed to topic: %s", topic)
+}
+
 func main() {
-	// Set the file name of the configurations file
-	viper.SetConfigName("config")
-	// Add the path to look for the configurations file
-	viper.AddConfigPath(".")
-	// Set the file type of the configurations file
-	viper.SetConfigType("yaml")
+	// Read the configuration file
+	var configuration = c.ReadDB()
 
-	// Enable VIPER to read Environment Variables
-	viper.AutomaticEnv()
+	//fmt.Println("Database is\t", configuration.Database.DBName)
+	//fmt.Println("Database is2\t", viper.GetString("database.dbname"))
 
-	var configuration c.Configurations
-
-	// Read in the configurations file and check for errors
-	if err := viper.ReadInConfig(); err != nil {
-		fmt.Printf("Error reading config file, %s\n", err)
-		panic(err)
+	//Set up mqtt client
+	var broker = configuration.Mqtt.Broker
+	var port = configuration.Mqtt.Port
+	fmt.Printf("Connecting to: tcp://%s:%d\n", broker, port)
+	client := mqtt.NewClient(mqtt.NewClientOptions().
+		AddBroker(fmt.Sprintf("tcp://%s:%d", broker, port)).
+		SetClientID("go-simple").
+		SetDefaultPublishHandler(messagePubHandler).
+		SetOnConnectHandler(connectHandler).
+		SetConnectionLostHandler(connectLostHandler),
+	)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
 	}
 
-	// set default values
-	viper.SetDefault("database.dbname", "test")
-
-	if err := viper.Unmarshal(&configuration); err != nil {
-		fmt.Printf("Unable to decode into struct, %v", err)
+	if token := client.Subscribe("ref/#", 1, nil); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
 	}
 
-	fmt.Println("Database is\t", configuration.Database.DBName)
-	fmt.Println("Database is\t", viper.GetString("database.dbname"))
+	for flag == false {
+		time.Sleep(1 * time.Second)
+	}
 
-	//
+	if token := client.Unsubscribe("ref/#"); token.Wait() && token.Error() != nil {
+		fmt.Println(token.Error())
+		os.Exit(1)
+	}
+
+	client.Disconnect(250)
 }
